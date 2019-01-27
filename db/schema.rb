897078fb9cd6cfb904429da7cd5b3a1b28c3449f
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2019-01-27 14:13:37 -0200

    added name to event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190127025018) do
+ActiveRecord::Schema.define(version: 20190127160803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20190127025018) do
     t.string "buy_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "artist_has_events", "artists"
