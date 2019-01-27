ca42cb6c1fee52664c9cb4391ff4d06c9ea66081
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2019-01-27 14:17:25 -0200

    added price to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190127160803) do
+ActiveRecord::Schema.define(version: 20190127161404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20190127160803) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "price"
   end
 
   add_foreign_key "artist_has_events", "artists"
