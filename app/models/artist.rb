e42e5da82b83b7ef4c7ddeab581f8df60839a8f9
Renan Lobato Rheinboldt <renan.lobato.rheinboldt@gmail.com>
2019-01-27 00:43:33 -0200

    annotate

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id                   :integer          not null, primary key
+#  name                 :string
+#  facebook_link        :string
+#  intagram_link_string :string
+#  spotify_link         :string
+#  donation_link        :string
+#  hire_link            :string
+#  genre                :string
+#  profile_picture      :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
 class Artist < ApplicationRecord
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id            :integer          not null, primary key
+#  date          :string
+#  location      :string
+#  event_picture :string
+#  buy_link      :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Event < ApplicationRecord
 end
